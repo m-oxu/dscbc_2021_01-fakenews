c881d33a30dc909bdc855a6b90ba0052c9894c37
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-10 23:02:08 -0300

    Update predict.py

--- AppVeritas/predict.py
+++ AppVeritas/predict.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from functions import ProjectFunctions
 import sklearn
-model = load(open('/app/dscbc_2021_01-fakenews/App_Veritas/model.joblib', 'rb'))
+model = load(open('/app/dscbc_2021_01-fakenews/AppVeritas/model.joblib', 'rb'))
 class Predict:
     
     def predict(txt):
