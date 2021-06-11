27788ebf6d990c39569e5d7a1ab39c81dc844883
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-10 22:57:28 -0300

    Update apresentacao.py

--- AppVeritas/apresentacao.py
+++ AppVeritas/apresentacao.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 def veritas():
     #st.image("/app/dscbc_2021_01-fakenews/App_Veritas/data/detector.jpg", use_column_width=True, width=[int])
-    st.image("/app/dscbc_2021_01-fakenews/App_Veritas/data/nossogrupo.JPG", use_column_width=True, width=[int])
+    st.image("/app/dscbc_2021_01-fakenews/AppVeritas/data/nossogrupo.JPG", use_column_width=True, width=[int])
     #st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/desenho.jpg", use_column_width=True, width=[int])
