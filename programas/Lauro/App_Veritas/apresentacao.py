fa74e207de4282f3b9c996411cee6b32f74a572d
Dairenkon Majime Rezende de Souza <dairenkonmajime@gmail.com>
2021-06-10 22:49:24 -0300

    Movendo diretório para frente

--- programas/Lauro/App_Veritas/apresentacao.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-
-def veritas():
-    #st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/detector.jpg", use_column_width=True, width=[int])
-    st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/nossogrupo.JPG", use_column_width=True, width=[int])
-    #st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/desenho.jpg", use_column_width=True, width=[int])
--- programas/Lauro/App_Veritas/cartilha.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-def veritas():
-    st.image("/app/dscbc_2021_01-fakenews/programas/Lauro/App_Veritas/data/IMG-20210610-WA0037.jpg", use_column_width=True, width=[int])
-
